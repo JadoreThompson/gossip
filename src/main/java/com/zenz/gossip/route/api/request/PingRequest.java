566cc46f77743e4426cbe7599959c66e36aed4eb
LucioJamieAI <148231849+JadoreThompson@users.noreply.github.com>
2026-04-10 19:24:48 +0100

    Updated payload property in PingRequest to hold list of Message objects

--- src/main/java/com/zenz/gossip/route/api/request/PingRequest.java
+++ src/main/java/com/zenz/gossip/route/api/request/PingRequest.java
@@ -1,15 +1,19 @@
 package com.zenz.gossip.route.api.request;
 
+import com.zenz.gossip.message.Message;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Getter
 @RequiredArgsConstructor
-public class PingRequest<T> {
+public class PingRequest {
 
     private final RequestType type =  RequestType.PING;
 
     private final String nodeId;
 
-    private final T payload;
+    private final List<Message> payload = new ArrayList<>();
 }
