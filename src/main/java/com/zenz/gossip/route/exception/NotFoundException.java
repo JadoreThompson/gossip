6ba6bde20f5ee8e7ed022f69afd92d6a9bca252a
LucioJamieAI <148231849+JadoreThompson@users.noreply.github.com>
2026-04-10 19:12:53 +0100

    Created GlobalExceptionHandler

---
+++ src/main/java/com/zenz/gossip/route/exception/NotFoundException.java
@@ -0,0 +1,8 @@
+package com.zenz.gossip.route.exception;
+
+public class NotFoundException extends RuntimeException {
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
