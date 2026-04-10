0b5b5e29e305272072270f3cf04130fb42f36077
LucioJamieAI <148231849+JadoreThompson@users.noreply.github.com>
2026-04-10 19:25:09 +0100

    Added properties to PongRequest

--- src/main/java/com/zenz/gossip/route/api/request/PongRequest.java
+++ src/main/java/com/zenz/gossip/route/api/request/PongRequest.java
@@ -1,4 +1,13 @@
 package com.zenz.gossip.route.api.request;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public class PongRequest {
+
+    private final RequestType type =  RequestType.PONG;
+
+    private final String nodeId;
 }
